538c33f3921ce62e8df01f3c9c70f5ad0a03432b
Aditya Vashistha <77147323+Aditya798-create@users.noreply.github.com>
2022-01-04 06:21:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
   // typing text animation script
   var typed = new Typed(".typing", {
-      strings: ["Learner", "Creator", "Student"],
+      strings: ["Learner", "Creator", "Student","Youtuber"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
